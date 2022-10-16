d7be6d7cd8678c06cb15255ced8431c4075a3b60
Patrik Kukic <patrikkukic@gmail.com>
2022-10-16 11:17:13 +0200

    implemented do_actions

--- analizator/LA.py
+++ analizator/LA.py
@@ -26,6 +26,9 @@ class Lex:
 
         self.input_string = ""
         self.length_of_input = 0
+        self.current_row = 1
+
+        self.output = []
 
         return
 
@@ -58,13 +61,29 @@ class Lex:
 
     def __set_active_enkas(self):
         self.active_enkas = {k:(v, -inf) for (k, v) in self.enkas_dict.items() if k.startswith(self.current_state)}
+        return
+
+    def __do_actions(self, list_of_actions, lexem):
+        for i, action in enumerate(list_of_actions):
+            if i == 0:
+                unif_char = action
+                if unif_char != '-':
+                    self.output.append(unif_char, self.current_row, lexem)
+
+            if 'NOVI_REDAK' in action:
+                self.current_row += 1
+            elif 'UDJI_U_STANJE' in action:
+                to_enter = action.split(' ')[1]
+                self.current_state = to_enter
+                self.__set_active_enkas()
+            elif 'VRATI_SE' in action:
+                to_group = int(action.split(' ')[1])
+                self.current_pos = self.start_of_expression + to_group
+                self.__set_active_enkas()
+        
+        return
 
     def compute_from_string(self, input_string):
-        # initialize pointers
-        # move pointers
-        # pointer array (for each enka)
-        # once all enka have terminated, remember the one with the top priority
-
         self.input_string = input_string
         self.length_of_input = len(input_string)
 
@@ -79,14 +98,16 @@ class Lex:
                     self.active_enkas[state][1] = self.current_pos
 
             if all(self.active_enkas.values(), lambda tup: tup[0].is_in_end_state()):
-                furthest_acceptable_state_at = max(self.active_enkas.values(), lambda tup: tup[1])
                 if self.current_pos != self.length_of_input - 1:
-                    # oporavak od pogreske
-                    pass
+                    self.current_pos = self.start_of_expression + 1
+                    self.start_of_expression = self.current_pos
+                    self.current_state = self.start_state
+                    self.__set_active_enkas()
                 else:
-                    # formiraj leksem
-                    # napravi akcije
-                    pass
+                    enka_name, (enka, furthest_pos) = max(self.active_enkas.items(), lambda tup: tup[1][1])
+                    lexem = self.input_string[self.start_of_expression:(furthest_pos + 1)]
+                    list_of_actions = self.actions_dict[enka_name]
+                    self.__do_actions(list_of_actions, lexem)
         return
 
 
