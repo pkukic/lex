3b0eb49209ed7c2ee1262ca4905f537c0627bd57
Patrik Kukic <patrikkukic@gmail.com>
2022-10-16 10:42:50 +0200

    updates to LA

--- analizator/LA.py
+++ analizator/LA.py
@@ -1,3 +1,5 @@
+from cmath import inf
+import ntpath
 import sys
 import os
 
@@ -5,33 +7,87 @@ from SimEnka import Enka
 
 class Lex:
     def __init__(self, enka_definitions_dir):
-        self.enka_dict = Lex.__load_enkas_from_definitions_dir(enka_definitions_dir)
+        self.actions_fname = ""
+        self.enka_definitions_fnames = []
+        self.enkas_dict = {}
+        
+        self.actions_dict = {}
+        self.start_state = ""
 
-        # load start state
-        # load actions
+        self.__get_fnames_from_dir(enka_definitions_dir)
+        self.__load_enkas()
+        self.__load_actions()
 
-    @classmethod
-    def __load_enkas_from_definitions_dir(definitions_dir):
-        definitions_dir_abs = os.path.abspath(definitions_dir)
-        fnames = list(os.listdir(definitions_dir_abs))
-        enkas = {}
-        for fname in fnames:
-            abs_fname = os.path.join(definitions_dir_abs, fname)
-            without_file_extension, _ = fname.split('.')
-            state, number = without_file_extension.split('_')
-            with open(abs_fname, 'r') as enka_definition:
-                enkas[f"{state}_{number}"] = Enka(enka_definition.read().splitlines())
-        return enkas        
+        self.current_state = self.start_state
+        self.start_of_expression = 0
+        self.end_of_expression = inf
+        self.current_pos = 0
+        self.active_enkas = {}
 
-    # def parse_actions(...)
+        self.input_string = ""
+        self.length_of_input = 0
 
-    def compute_from_string(input_string):
+        return
+
+    def __get_fnames_from_dir(self, dir):
+        dir_abspath = os.path.abspath(dir)
+        abs_fnames = [os.path.join(dir_abspath, fname) for fname in os.listdir(dir_abspath)]
+        for fname in abs_fnames:
+            if 'akcije' in ntpath.basename(fname):
+                self.actions_fname = fname
+            else:
+                self.enka_definitions_fnames.append(fname)
+        return
+
+    def __load_enkas(self):
+        for fname in self.enka_definitions_fnames:
+            with open(fname, 'r') as definition:
+                enka_name = ntpath.basename(fname).split('.')[0]
+                self.enkas_dict[enka_name] = Enka(definition.read().splitlines())
+        return
+
+    def __load_actions(self):
+        with open(self.actions_fname, 'r') as actions_defitions:
+            lines = actions_defitions.read().splitlines()
+            self.start_state = lines[0]
+            for line in lines[1:]:
+                state, list_of_actions = line.split(':')
+                list_of_actions = list_of_actions.split(',')
+                self.actions_dict[state] = list_of_actions
+        return
+
+    def __set_active_enkas(self):
+        self.active_enkas = {k:(v, -inf) for (k, v) in self.enkas_dict.items() if k.startswith(self.current_state)}
+
+    def compute_from_string(self, input_string):
         # initialize pointers
         # move pointers
         # pointer array (for each enka)
         # once all enka have terminated, remember the one with the top priority
 
-        pass
+        self.input_string = input_string
+        self.length_of_input = len(input_string)
+
+        while self.current_pos < self.length_of_input:
+            
+            self.__set_active_enkas()
+            c = self.input_string[self.current_pos]
+
+            for state in self.active_enkas:
+                self.active_enkas[state][0].feed_next_character(c)
+                if self.active_enkas[state][0].is_in_acceptable_state():
+                    self.active_enkas[state][1] = self.current_pos
+
+            if all(self.active_enkas.values(), lambda tup: tup[0].is_in_end_state()):
+                furthest_acceptable_state_at = max(self.active_enkas.values(), lambda tup: tup[1])
+                if self.current_pos != self.length_of_input - 1:
+                    # oporavak od pogreske
+                    pass
+                else:
+                    # formiraj leksem
+                    # napravi akcije
+                    pass
+        return
 
 
 if __name__ == '__main__':
