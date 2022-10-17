72cbf91be6bedb450b814732f4751eb9c2c433b4
Patrik Kukic <patrikkukic@gmail.com>
2022-10-17 15:56:14 +0200

    GLA working

--- regex_prep.py
+++ regex_prep.py
@@ -2,9 +2,10 @@ import sys
 
 class RegexPrep:
     
-    def __init__(self, target: str):
+    def __init__(self, target: str, input_file):
         self.target = target
         self.prev_regex = {}
+        self.input_file = input_file
         return
     
 
@@ -17,31 +18,32 @@ class RegexPrep:
 
     def start(self) -> str:
         beginning_state = ""
-        with open(self.target, 'w') as wf:
-            for line in sys.stdin.readlines():
-                if line.startswith("{") and line != "{\n":
-                    # for regex definitions
-                    index = line.find(" ")
-                    name, reg = line[:index + 1], line[index + 1:-1] # remove the \n from the end
-                    processed = self.change_references_to_regex(reg)
-                    self.prev_regex.update({name[:-1]: processed})
-                elif line.startswith("%X"):
-                    parsed = line.split(" ")
-                    beginning_state = parsed[1]
-                    continue
-                elif line.startswith("%L"):
-                    continue
-                elif line.startswith("<"):
-                    # for actions
-                    index = line.find(">")
-                    new_line = line[0:index + 1]
-                    processed = self.change_references_to_regex(line[index + 1:-1])
-                    new_line = new_line + processed
-                    if not new_line.endswith("\n"):
-                        new_line = new_line + "\n"
-                    wf.write(new_line)
-                else:
-                    new_line = line
-                    wf.write(new_line)
+        with open(self.input_file, 'r') as rf:
+            with open(self.target, 'w+') as wf:
+                for line in rf.readlines():
+                    if line.startswith("{") and line != "{\n":
+                        # for regex definitions
+                        index = line.find(" ")
+                        name, reg = line[:index + 1], line[index + 1:-1] # remove the \n from the end
+                        processed = self.change_references_to_regex(reg)
+                        self.prev_regex.update({name[:-1]: processed})
+                    elif line.startswith("%X"):
+                        parsed = line.split(" ")
+                        beginning_state = parsed[1]
+                        continue
+                    elif line.startswith("%L"):
+                        continue
+                    elif line.startswith("<"):
+                        # for actions
+                        index = line.find(">")
+                        new_line = line[0:index + 1]
+                        processed = self.change_references_to_regex(line[index + 1:-1])
+                        new_line = new_line + processed
+                        if not new_line.endswith("\n"):
+                            new_line = new_line + "\n"
+                        wf.write(new_line)
+                    else:
+                        new_line = line
+                        wf.write(new_line)
         return beginning_state
                     
